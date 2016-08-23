dcac688f8f4f0d09ff0465e0cea14d519824c2b5
kaylynyuh <yuhkay4@gmail.com>
2016-08-23 11:33:16 -0700

    added IIFEs

--- js/controllers/navController.js
+++ js/controllers/navController.js
@@ -1,9 +1,13 @@
-function openNav() {
-  document.getElementById('mySidenav').style.width = '8em';
-  document.getElementById('main').style.marginLeft = '8em';
-}
+(function () {
 
-function closeNav() {
-  document.getElementById('mySidenav').style.width = '0';
-  document.getElementById('main').style.marginLeft= '0';
-}
+  function openNav() {
+    document.getElementById('mySidenav').style.width = '8em';
+    document.getElementById('main').style.marginLeft = '8em';
+  }
+
+  function closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
+
+})(window);
